989884a06036233f2ee5f108b9567d338ccfb9cd
Joshua Zenn <wordman05@gmail.com>
2015-06-28 17:43:29 -0400

    Create mail.rb

---
+++ inc/mail.rb
@@ -0,0 +1,16 @@
+require 'pony'
+
+module Mail
+  def Mail.send(to, subject, body)
+    Pony.mail :to => to,
+              :from => "me@example.com",
+              :subject => subject,
+              :body => body
+  end
+  def Mail.sendtemp(to, subject, bodypath)
+    Pony.mail :to => to,
+              :from => "me@example.com",
+              :subject => subject,
+              :body => slim bodypath.to_sym
+  end
+end
