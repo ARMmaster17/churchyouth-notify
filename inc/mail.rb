b1cd0d89148a9f882519951098728d2d8fca2a43
Joshua Zenn <wordman05@gmail.com>
2015-06-28 18:35:14 -0400

    Update mail.rb

--- inc/mail.rb
+++ inc/mail.rb
@@ -7,12 +7,6 @@ module Mail
               :subject => subject,
               :body => body
   end
-  def Mail.sendtemp(to, subject, bodypath)
-    Pony.mail :to => to,
-              :from => "me@example.com",
-              :subject => subject,
-              :body => slim bodypath.to_sym
-  end
   def Mail.validate(to)
     #Validate email address
     return true
