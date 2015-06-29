1176bc02b3f0da2445812e889037e14dadba418d
Joshua Zenn <wordman05@gmail.com>
2015-06-29 10:47:36 -0400

    Update and rename config.json to config.rb

---
+++ config/config.rb
@@ -0,0 +1,13 @@
+Module Config
+  WARD = "Place Holder Ward"
+  WARDID = 000000000000
+  QUORUMDEACON = true
+  QUORUMTEACHER = true
+  QUORUMPRIEST = true
+  CLASSBEHIVE = false
+  CLASSMIAMAID = false
+  CLASSLAUREL = false
+  ROOTURL = "http://ldsyouth-notify.herokuapp.com"
+  SSL = false
+  LDSIO = falsee
+end
