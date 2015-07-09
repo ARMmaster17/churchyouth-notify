d50f9459303f7f3f0929d251adc7fb6c42519580
Joshua Zenn <wordman05@gmail.com>
2015-07-09 14:33:46 -0400

    Update config.rb

--- inc/config.rb
+++ inc/config.rb
@@ -1,10 +1,17 @@
 require_relative '../config/configstore'
+require 'json'
 
 module Config
   def Config.getVar(varname)
     case varname
     when "ward"
-      return ConfigStore::WARD
+      begin
+        file = open("shops.json")
+        parsed = JSON.parse(file.read)
+      rescue
+        return "Error"
+      end
+      return parsed['ward']
     else
       return "Empty"
     end
