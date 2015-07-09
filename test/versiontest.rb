a3fd4c0f240e08fd420ecdc58aa59d9ce1a89118
Joshua Zenn <wordman05@gmail.com>
2015-07-09 15:12:12 -0400

    Update versiontest.rb

--- test/versiontest.rb
+++ test/versiontest.rb
@@ -13,7 +13,7 @@ class TestVersion < Test::Unit::TestCase
     assert last_response.ok?
   end
   def test_setupcallback
-    post '/setupcallback', :ward => "Test ward", :qa => ["qd","qt","qp"], :ca => ["cb","cm","cl"]
+    post '/setup/callback', :ward => "Test ward", :qa => ["qd","qt","qp"], :ca => ["cb","cm","cl"]
     assert !last_response.ok?
     puts last_response
     assert File.exists?("../config/config.json")
