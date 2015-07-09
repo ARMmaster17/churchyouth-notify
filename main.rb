f5ae0e8cfc56b29b80c85473d9fd0f87dcaf762e
Joshua Zenn <wordman05@gmail.com>
2015-07-09 14:28:06 -0400

    Update main.rb

--- main.rb
+++ main.rb
@@ -43,9 +43,24 @@ get '/setup' do
 end
 post '/setup/callback' do
   # Send configuration to config file
-  classquorumarray = params[:cq]
+  quorumarray = params[:qa]
+  classarray = params[:ca]
   warddata = params[:ward]
-  #TODO: add interface to config file
+  quorumstring = quorumarray.map(&:inspect).join(',')
+  classstring = classarray.map(&:inspect).join(',')
+  begin
+    file = File.open("/config/config.json", "w")
+    file.write('{ "ward":"#{warddata}","classes":{ #{classstring} },"quorums":{ #{quorumstring} }}') 
+    file.close()
+  rescue IOError => e
+    "An error has occured. Please send this code to the developer: 0x00 CONFIGSTOREIOERR"
+    ioerr = true
+  ensure
+    file.close unless file.nil?
+  end
+  if !ioerr
+    redirect "/"
+  end
 end
 get '/api' do
   API.a()
