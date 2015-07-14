17e8fa55e8dcfd4e35e1d7c84d5c1a68259a3f6d
Joshua Zenn <wordman05@gmail.com>
2015-07-14 09:17:04 -0400

    Update ctrlreclist.js

--- public/src/js/dashboard/controllers/ctrlreclist.js
+++ public/src/js/dashboard/controllers/ctrlreclist.js
@@ -1,10 +1,11 @@
 angular.module('appRecList', []).controller('ctrlRecList', function($scope) {
     $scope.modRecList = [
-        {uid:'1',fname:'Test',lname:'User',gid:'p',email:'user@example.com'},
-        {uid:'2',fname:'Test',lname:'User',gid:'p',email:'user@example.com'},
-        {uid:'3',fname:'Test',lname:'User',gid:'p',email:'user@example.com'},
-        {uid:'4',fname:'Test',lname:'User',gid:'p',email:'user@example.com'},
-        {uid:'5',fname:'Test',lname:'User',gid:'p',email:'user@example.com'},
-        {uid:'6',fname:'Test',lname:'User',gid:'p',email:'user@example.com'}
+        {'uid':'1','fname':'Test','lname':'User','gid':'p','email':'user@example.com'},
+        {'uid':'2','fname':'Test','lname':'User','gid':'p','email':'user@example.com'},
+        {'uid':'3','fname':'Test','lname':'User','gid':'p','email':'user@example.com'},
+        {'uid':'4','fname':'Test','lname':'User','gid':'p','email':'user@example.com'},
+        {'uid':'5','fname':'Test','lname':'User','gid':'p','email':'user@example.com'},
+        {'uid':'6','fname':'Test','lname':'User','gid':'p','email':'user@example.com'},
+        {'uid':'7','fname':'Test','lname':'User','gid':'p','email':'user@example.com'}
     ];
 });
